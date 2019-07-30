9e4354b308c9b8bc84357d008c56376477919853
wangxin1009 <wangxinpm1009@gmail.com>
2019-07-30 23:32:11 +0800

    react material ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+import Bookmarks from '@material-ui/icons/Bookmarks';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Button variant = "outlined" color="primary">
+        <Bookmarks></Bookmarks>Chapter 2
+      </Button>
     );
   }
 }
